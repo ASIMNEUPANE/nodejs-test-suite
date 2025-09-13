0324065c68fc4ccdaf6df6e4ae0a4f0e901a99d0
asim <asimneupane11@gmail.com>
2025-09-13 14:56:18 +0545

    test-api-async-req

--- src/tests/sum.test.ts
+++ src/tests/sum.test.ts
@@ -1,3 +1,14 @@
-import {describe,test,expect,it} from '@jest/globals'
+import { describe, test, expect, it } from '@jest/globals'
 import request from 'supertest'
-import {app} from '../index'
+import { app } from '../index'
+
+describe('test sum api', () => {
+  it('test 1 +3', async () => {
+    const res = await request(app).post('/sum').send({
+      a: 1,
+      b: 2,
+    })
+    expect(res.statusCode).toBe(200)
+    expect(res.body.answer).toBe(3)
+  })
+})
